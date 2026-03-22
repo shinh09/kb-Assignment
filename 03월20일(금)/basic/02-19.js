aab99a0ebf698b7dceb6e9d08b711dcd9e768890
shinhyung <23102009@seoultech.ac.kr>
2026-03-23 01:50:46 +0900

    0320 - 템플릿,디렉티브

---
+++ 03월20일(금)/basic/02-19.js
@@ -0,0 +1,6 @@
+// 02-18까지 03월16일 폴더에 있음..!!
+let base = 100;
+const add = (x) => base + x;
+const multiply = (x) => base * x;
+//방법1
+export { add, multiply };
---
+++ 03월20일(금)/basic/02-20-module.js
@@ -0,0 +1,6 @@
+let base = 100;
+
+export const add = (x) => base + x;
+export const multiply = () => base * x;
+
+export default () => getBase;
