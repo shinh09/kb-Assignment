c3a108a1cb3ce63bf09815465aa5639f3f6c6386
shinhyung <23102009@seoultech.ac.kr>
2026-04-20 20:40:48 +0900

    0416 - 조건문,반복문,참조타입

---
+++ 04월02일(목)/axios-test-app/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import axios from 'axios';
+
+const requestAPI = () => {
+  const url = '/api/todos/2';
+
+  axios.get(url).then((response) => {
+    console.log('#응답객체:', response);
+  });
+};
+requestAPI();
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>콘솔확인하기</p>
+</template>
+
+<style scoped></style>
---
+++ 4월06일(월)/pinia-todo-app/src/App.vue
@@ -0,0 +1,31 @@
+<script setup></script>
+
+<template>
+  <h1>TodoList 테스트(composition API)!</h1>
+  <span><input type="text" /><button>추가</button></span>
+  <ul>
+    <li>
+      예시 할일
+      <button>삭제</button>
+    </li>
+  </ul>
+  <p>완료된 할일 수 : {{}}</p>
+</template>
+
+<style scoped></style>
+
+<!-- 
+1. pinia 인스턴스 만들기 (stores/counter.js)
+2. 등록하기 app.use(pinia)
+3. 
+-->
+
+<!-- 
+데이터: 
+1. todoList (+ completed인지 아닌지)
+2. countComplete
+동작 : 
+1. delete
+2. complete
+3. AddTodo
+-->
